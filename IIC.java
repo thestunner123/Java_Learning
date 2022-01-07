26c097e70adfff6b7277063588d081d8b56fa715
theStunner123 <sahilacharya5555@gmail.com>
2022-01-07 18:31:18 +0545

    KJK

---
+++ IIC.java
@@ -0,0 +1,13 @@
+public class IIC{
+public static void main(String[]args){
+String name="sahil";
+System.out.println(name);
+long phone= 9819067945l;
+System.out.println(phone);
+String address= "biratnagar_t2";
+System.out.println("address");
+boolean ismarried = false;
+System.out.println("is married");
+char gender= 'm';
+System.out.println("gender");
+}}
