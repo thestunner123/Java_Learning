c79ad4c118e3c54caed1ea079d14c92434678215
theStunner123 <sahilacharya5555@gmail.com>
2022-01-07 18:22:29 +0545

    "Fixing of errors"

--- main.java
+++ main.java
@@ -3,9 +3,9 @@ import javax.sound.sampled.SourceDataLine;
 public class main{
 public static void main(String[]args){
 float a,b,c,d, perimeter;
-l=5;
-b=4;
-perimeter=2*(l+b);
+ a=5;
+ b=4;
+perimeter=2*(a+b);
 System.out.println("Perimeter of rentangle is:"+perimeter);
 
 }}
--- ternary.java
+++ ternary.java
@@ -1,4 +1,4 @@
-public class  TernaryG{
+public class  ternary{
 public static void main (String [] args){
 int a = 10;
 int b  = 6;
